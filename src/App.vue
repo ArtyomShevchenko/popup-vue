d6d0f46b180b95cfde7fcb46b4cb22ed1cab8e22
Admin <artyomshevchenkowarmgray@gmail.com>
2022-12-13 15:44:35 +0200

    Create simple popup

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,109 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template ref="app">
+  <h1>Vue popups</h1>
+  <button @click="openPopup">Open popup</button>
+  <Popup ref="popupInfo">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet delectus ea expedita ab tenetur rem, quas non
+    aliquid earum, iste inventore minima accusantium consequuntur obcaecati saepe vitae eum nesciunt. Quos.
+  </Popup>
+
+  <Popup ref="popupGreating">
+    Hello,
+    <br />you are on 
+    <br />Artem's website.
+  </Popup>
+
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum assumenda, vero voluptates, corporis
+    perspiciatis eius provident expedita omnis placeat earum hic ducimus odit quidem doloremque! Sit iste vitae possimus
+    quia dolorem itaque. Sed quaerat fuga odit quos voluptas, quam nam asperiores delectus provident architecto facilis
+    illum maxime? Quaerat voluptates enim nisi earum qui illo aliquam quidem perferendis velit odio nesciunt odit,
+    dolorem ipsa omnis fugit voluptate quod et sequi! Excepturi sapiente, itaque explicabo et quia nemo doloremque cum
+    quibusdam cupiditate at optio a maxime dignissimos blanditiis vero alias delectus id voluptas quos iure harum. Aut
+    id quibusdam accusamus tempore aliquam quam commodi optio, porro consectetur in sequi quis dignissimos minus ex ad
+    libero ea, quia neque repellat quod nam. Sunt reiciendis temporibus odio perspiciatis non quos ab itaque
+    reprehenderit voluptatum porro, beatae corporis accusamus, sed deserunt, laudantium tempore excepturi officia. Fuga
+    quas asperiores minus obcaecati dolor repellendus neque laborum possimus hic alias fugiat ipsa distinctio voluptas
+    praesentium tempore doloremque, molestiae vitae quasi? Eligendi, distinctio eos minus soluta accusamus unde
+    similique et minima odit aliquam esse veritatis natus obcaecati voluptate? Saepe ab expedita quae atque inventore ex
+    autem officiis placeat. Fugit maiores est molestias dolorem tempore velit dolor impedit provident mollitia animi,
+    sed ea amet voluptatibus, perspiciatis facilis nobis ratione minus eaque officiis et voluptate obcaecati? Doloribus
+    blanditiis iusto amet error sequi magni cum, maxime libero, est ratione molestias dolorem dolor, itaque dolores esse
+    cupiditate magnam dolorum. Repellat qui tempore commodi eveniet facere odio aspernatur, vitae, laudantium itaque sit
+    numquam illum! Vero voluptate minus velit hic sit sapiente atque expedita dolor praesentium pariatur eos dolorem
+    illum deserunt, ad, ipsam earum inventore repellendus porro quo ut blanditiis. Tempora deleniti iste nostrum, aut ab
+    laborum dignissimos explicabo quae sapiente delectus repudiandae, totam dolorum sequi earum facere accusantium quia.
+    Error velit maxime magni in accusamus aspernatur ipsum, id modi enim minus quod illo mollitia neque corporis
+    explicabo? Impedit eos sed cum delectus quibusdam culpa repellendus quia deserunt aut possimus consequatur, itaque
+    expedita. Libero ipsum nemo nihil repudiandae maxime minima dolorum architecto, esse similique vitae rerum alias
+    voluptates aliquam saepe nam commodi nisi qui labore? Minima numquam cumque, praesentium eveniet omnis, tempora
+    quisquam sunt quasi consequatur, repellendus sint non! Saepe perspiciatis deserunt ut odit doloribus optio
+    perferendis quas? Nobis quaerat eius voluptate tenetur sit iusto molestias, facere labore deleniti, maiores
+    reiciendis culpa quas. Soluta expedita laborum delectus temporibus, iure, culpa tempora eligendi neque unde iusto
+    sunt vero ipsum repellendus impedit, officiis perspiciatis id optio exercitationem at atque quas! Doloribus vero
+    amet incidunt facere odit asperiores alias natus assumenda et minus eaque placeat perferendis, ipsa cupiditate
+    corporis in. Fuga officia porro blanditiis fugiat laborum illo molestiae maxime necessitatibus dolores quasi a vel
+    doloribus, eligendi, voluptatum at? Neque ut facere sequi praesentium qui officia eveniet aperiam. Hic vel
+    perferendis expedita labore, maxime nihil sapiente. Laboriosam suscipit quam voluptate commodi at porro corrupti
+    quisquam iure, alias praesentium dolorum cupiditate quia nobis amet aliquid! Qui ducimus dolore nulla quidem! Porro,
+    deleniti ipsa asperiores magnam dolorem enim eos, tempora dolores voluptate nam modi repudiandae ut unde quisquam
+    numquam nisi praesentium amet consequatur, inventore minima. Eum, quod, deserunt commodi dolor repellendus iusto nam
+    voluptatibus amet sint magni tempora repellat dolore similique earum consectetur doloribus nisi. Aliquid, vitae
+    sequi veritatis excepturi tenetur praesentium natus fugiat molestias pariatur consequatur veniam iure inventore vero
+    sit enim. Necessitatibus porro veritatis culpa obcaecati quo temporibus. Voluptates numquam, officiis laborum
+    quibusdam sit consectetur fuga dignissimos quasi, quas vitae voluptatibus aspernatur placeat provident nulla tempore
+    tempora esse laudantium facere perspiciatis eligendi reprehenderit accusamus delectus dolorem iste! Laudantium in a
+    beatae distinctio dolorum delectus quos perferendis tenetur aspernatur ipsa, cupiditate cumque molestiae esse unde
+    autem perspiciatis vel suscipit omnis totam! Obcaecati cum voluptates est dicta odit eveniet, dignissimos nobis,
+    optio aliquam suscipit ab odio quidem ratione atque ex vero similique fugiat magni quasi. Suscipit sunt soluta quo
+    doloremque voluptate voluptatibus beatae cupiditate consectetur magni, odio odit quam tenetur cum animi eius non,
+    optio similique. Similique voluptate neque autem ipsum perspiciatis tenetur provident quisquam dolores, nihil soluta
+    quo sint delectus non quaerat aspernatur veritatis porro. Quaerat optio, corrupti facilis veritatis veniam est nemo!
+    Quis soluta laudantium nobis. Distinctio, blanditiis dignissimos molestiae aut ratione qui voluptas beatae ducimus
+    deserunt atque officia vero perspiciatis harum, quisquam error? Facilis sunt quidem fuga ipsam? Assumenda beatae id
+    incidunt ipsa cupiditate sint dolor reiciendis labore sequi voluptate? Hic nobis dolor saepe exercitationem porro
+    perferendis voluptate ipsum odio dolorum quos eveniet reiciendis, quia quo veniam, numquam ipsa adipisci dignissimos
+    tempora nihil, perspiciatis vitae provident eum. Expedita reiciendis ullam cum rerum ipsum nobis provident voluptas
+    eos voluptatibus, doloremque hic. Omnis repudiandae sapiente, veritatis dicta earum consectetur corrupti doloremque
+    facilis voluptas, unde assumenda exercitationem nam, distinctio praesentium deserunt asperiores! Officiis dolorem
+    dolore eos velit nesciunt, debitis nam aspernatur enim in, culpa dicta accusantium necessitatibus voluptate impedit
+    ipsam voluptatibus deserunt aliquid. Ad id cupiditate earum dicta veritatis. Modi eos eligendi nulla esse cumque
+    illum quis adipisci, assumenda atque nam recusandae, accusantium asperiores voluptatibus suscipit rerum, maiores
+    illo delectus temporibus distinctio similique perspiciatis pariatur commodi impedit mollitia? Explicabo culpa
+    consequatur minus voluptatibus. Impedit commodi quas minus harum sint, maiores minima consectetur non facere ut.
+    Possimus consectetur id voluptate enim illo ipsam dolorem, quo impedit molestias atque magnam, odio, eius
+    exercitationem tenetur neque dolor eligendi vero. Numquam qui perspiciatis harum corrupti porro illo iusto! Tempore
+    omnis facilis veniam laboriosam dignissimos alias et placeat dolorem perspiciatis saepe delectus a, culpa doloribus
+    architecto voluptate error quis possimus aliquam? Fugit ullam modi in odio cum non culpa quos maiores quo libero
+    iusto ea eveniet doloremque, odit minus veniam ducimus nemo quibusdam dolores aliquam nam accusamus ex! Adipisci
+    exercitationem possimus cum assumenda sunt pariatur eum at? Adipisci pariatur nihil cumque atque consequatur?
+    Voluptates sequi nihil, asperiores necessitatibus culpa, placeat vero ullam quae dolores veniam eius eum delectus
+    ratione natus illum, repellat maxime. Exercitationem quo omnis laboriosam rem aut doloremque ex odio modi officiis,
+    voluptates architecto quos voluptatem. Possimus maiores sed natus temporibus esse nisi quam aliquid doloribus.
+    Placeat non laborum expedita quaerat distinctio dolorem blanditiis.</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Popup from "@/components/Popup.vue"
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components: { Popup },
+  methods: {
+    openPopup() {
+      this.$refs.popupInfo.open()
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.$refs.popupGreating.open()
+    }, 1000)
   }
 }
 </script>
 
 <style>
+.hidden-scroll {
+  overflow: hidden;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Popup.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="popup-container" v-if="isOpen" @click.self="close">
+        <!-- <div class=" poput-inner" @click.capture="open"> -->
+        <div class=" poput-inner">
+            <slot></slot>
+            <button @click="close">Close</button>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "PopupComponont",
+    data() {
+        return {
+            isOpen: false,
+        }
+    },
+    methods: {
+        open() {
+            this.isOpen = true
+        },
+
+        close() {
+            this.isOpen = false
+        },
+    },
+    watch: {
+        isOpen(value) {
+            if (value) {
+                document.querySelector("body").classList.add("hidden-scroll")
+            }
+
+            if (!value) {
+                document.querySelector("body").classList.remove("hidden-scroll")
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+.popup-container {
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: 99;
+    background-color: rgba(0, 0, 0, 0.5);
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.poput-inner {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    gap: 2rem;
+    background-color: #fff;
+    padding: 2rem;
+    max-width: 400px;
+}
+</style>
